822b77bee5665a7ac087e5c3dfd0831dd24364bd
이찬우 <overelectric@gmail.com>
2020-06-15 16:17:21 +0900

    Add index.html

--- crud/articles/views.py
+++ crud/articles/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
+# Article.objects를 위해 import 한다.
+from .models import Article
 
 # Create your views here.
 def index(request):
-    return render(request, 'articles/index.html')
+    # 데이터 베이스에서 모든 값을 뽑아온다.
+    articles = Article.objects.all()
+    context = {
+        'articles' : articles,
+    }
+    return render(request, 'articles/index.html',context)
