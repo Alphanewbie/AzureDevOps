90c0ccb861322b3a9ca21d6b361fb3f8a98dc239
이찬우 <overelectric@gmail.com>
2020-06-17 17:49:34 +0900

    Add detail

--- 02_django_form/crud_fork/pages/urls.py
+++ 02_django_form/crud_fork/pages/urls.py
@@ -5,4 +5,5 @@ app_name = 'pages'
 urlpatterns = [
     path('', views.index, name="index"),
     path('create/', views.create, name="create"),
+    path('<int:pk>/detail/', views.detail, name="detail"),
 ]
--- 02_django_form/crud_fork/pages/views.py
+++ 02_django_form/crud_fork/pages/views.py
@@ -3,19 +3,29 @@ from django.views.decorators.http import require_http_methods
 from .models import Pages
 from .forms import PagesForm
 
-def index(request):
-    return render(request,'pages/index.html')
 
-@require_http_methods(['POST','GET'])
+def index(request):
+    return render(request, 'pages/index.html')
+
+
+@require_http_methods(['POST', 'GET'])
 def create(request):
     if request.method == 'POST':
         form = PagesForm(request.POST)
         if form.is_valid():
-            form.save()
-            return redirect('pages:index')
+            page=form.save()
+            return redirect('articles/detail.html',page.pk)
     else:
         form = PagesForm()
     context = {
-        'form' : form,
+        'form': form,
     }
-    return render(request,'pages/create.html', context)
+    return render(request, 'pages/create.html', context)
+
+
+def detail(request, pk):
+    article = Article.objects.get(pk=pk)
+    context = {
+        'article': article,
+    }
+    return render(request, 'articles/detail.html', context)
